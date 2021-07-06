e512c82974df442546caff7a87b5c6a8c014902e
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-06 17:36:06 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,7 @@
 const Insta = require('@androz2091/insta.js');
 const Moe = new Insta.Client();
 const chatbot = require("node-fetch").default;
+
+Moe.on('connected', () => {
+    console.log(`I Am Ready`);
+});
